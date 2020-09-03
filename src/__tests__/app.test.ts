5aefeabd3c1d2fa0920d4b7b5b3fadf4090ca5b7
stand-sure <chris.anderson@zywave.com>
2020-09-03 14:12:55 -0400

    added snapshot test

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -7,18 +7,23 @@ type Node = JSX.Element & VNode<RendererNode, RendererElement>;
 import logo from "../assets/logo.png";
 
 describe("App", () => {
-  it("should use logo", () => {
-    const node = App() as Node;
-    const children = node?.children as Node[];
-    const img = children?.find(c => c.type === "img");
-    const actual = img?.props?.src;
-    expect(actual).toBe(logo);
-  });
+    it("should use logo", () => {
+        const node = App() as Node;
+        const children = node?.children as Node[];
+        const img = children?.find((c) => c.type === "img");
+        const actual = img?.props?.src;
+        expect(actual).toBe(logo);
+    });
 
-  it("should use Demo", ()=> {
-    const node = App() as Node;
-    const children = node?.children as Node[];
-    const demo = children?.find(c => c.type === "Demo");
-    expect(demo).not.toBeNull();
-  })
+    it("should use Demo", () => {
+        const node = App() as Node;
+        const children = node?.children as Node[];
+        const demo = children?.find((c) => c.type === "Demo");
+        expect(demo).not.toBeNull();
+    });
+
+    it("should render correctly", () => {
+      const tree = App();
+      expect(tree).toMatchSnapshot();
+    })
 });
